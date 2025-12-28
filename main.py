b2b0c85ea688fcc28c2f27be7aa867e0a1d62cd3
HienXuanPham <phamxuanhien02@gmail.com>
2025-12-27 21:26:02 -0500

    print activities

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import sys
 import urllib.request
+import json
 
 def main():
   if len(sys.argv) > 1:
-    user_name = sys.argv[1]
-    url = f"https://api.github.com/users/{user_name}/events"
+    username = sys.argv[1]
+    url = f"https://api.github.com/users/{username}/events"
+    map_events = {
+      "CreateEvent": "created",
+      "PushEvent": "pushed a commit to",
+      "PullRequestEvent": "opened a pull request in",
+      "IssueCommentEvent": "commented in"
+    }
 
     try:
       with urllib.request.urlopen(url) as response:
-        data = response.read()
-        print(data.decode())
+        response_bytes = response.read() # bytes
+        json_text = response_bytes.decode() # JSON
+        activities = json.loads(json_text)
+        
+        for event in activities:
+          t = event.get("type")
+          if t in map_events:
+            print(f"- {username} {map_events[t]} {event.get('repo', {}).get('name')}")
+
     except urllib.error.URLError as e:
       print(f"Error accessing URL: {e.reason}")
   else:
